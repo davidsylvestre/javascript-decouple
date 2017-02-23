096f2bd7cf0e17776bfd131b4b977d2eef23baa4
David Sylvestre <david.sylvestre@hotmail.com>
2017-02-23 13:36:45 -0300

    Abstração da função replace

--- support/helpers.js
+++ support/helpers.js
@@ -8,6 +8,8 @@ const split = curry((delimiter, str) => str.split(delimiter))
 const reduce = curry((initialValue, fn, arr) => arr.reduce(fn, initialValue))
 const reduce_to_object = reduce({})
 
+const replace = curry( (regex, newStr, str) => str.replace(regex, newStr) )
+
 const trim = str => str.trim()
 const isQuoted = str => (str.length > 0 && str.charAt(0) === '\"' && str.charAt(str.length  - 1) === '\"')
 
@@ -18,4 +20,4 @@ const expandNewLineQuoted = str => isQuoted(str) ? expandNewLine(str) : str
 const normalizeString = compose(removeQuotes)
 
 
-module.exports = {map, split, reduce, reduce_to_object, removeQuotes}
+module.exports = {map, split, reduce, reduce_to_object, replace, removeQuotes}
